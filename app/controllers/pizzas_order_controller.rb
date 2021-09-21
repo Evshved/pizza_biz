b4b902cab7909f7c490645b1c5852d19298526cc
Eugene Shved <shved.eugene@gmail.com>
2021-09-21 03:33:22 +0300

    Update assets, add pizzas order

--- app/controllers/pizzas_order_controller.rb
+++ app/controllers/pizzas_order_controller.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class PizzasOrderController < ApplicationController
-  def index
-    # @order_items = PizzaType.where(id: ids)
-    # @price = Discounts::PriceCalculator.new(price)
-    # respond_to { |format| format.html }
-  end
+  def index; end
 
   def create
     order_item_ids = []
@@ -21,5 +17,32 @@ class PizzasOrderController < ApplicationController
 
     order = Order.new(ids: order_item_ids, price: price)
     respond_to if order.save
+  end
+
+  def order_items
+    pizza_orders = []
+    permitted_params.each do |key, value|
+      pizza_types.select do |type|
+        pizza_orders << { name: type.name, price: type.price, quantity: value } if type.name == key.capitalize
+      end
+    end
+
+    respond_to do |format|
+      format.json { render json: { success: true, html: (render_to_string partial: 'pizzas_order/order_items.html.erb', locals: { pizza_orders: pizza_orders }) } }
+    end
+  end
+
+  private
+
+  def pizza_types
+    @pizza_types ||= PizzaType.all
+  end
+
+  def pizza_type_names
+    @pizza_type_names ||= pizza_types.pluck(:name).map { |e| e.downcase.to_sym }
+  end
+
+  def permitted_params
+    params.permit(pizza_type_names)
   end
 end

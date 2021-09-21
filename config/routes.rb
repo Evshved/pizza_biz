21e05e91f8b6442dd3f21476bb023f84719efafc
Eugene Shved <shved.eugene@gmail.com>
2021-09-21 03:36:31 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :pizzas_order, only: %i[index create]
+  resources :pizzas_order, only: %i[index create] do
+    collection do
+      post :order_items
+    end
+  end
   resources :pizza_menu, only: [:index]
   namespace :api do
     resources :pizza_orders, only: %i[index create]
